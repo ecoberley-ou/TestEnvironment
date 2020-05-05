ad7157524d9549a9391600f8bd170cbe215e600f
Evan Coberley <ecoberley@ou.edu>
2020-05-05 17:26:16 -0500

    Copied GearsException

---
+++ TestEnvironment/src/GearsException.java
@@ -0,0 +1,9 @@
+public class GearsException extends Exception{
+    private static final long serialVersionUID = 1L;
+
+    public GearsException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    
+}
