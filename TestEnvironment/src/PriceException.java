c08561cbb027084fb25086e7590eb26d53c9d285
Evan Coberley <ecoberley@ou.edu>
2020-05-05 17:27:09 -0500

    Copied PriceException

---
+++ TestEnvironment/src/PriceException.java
@@ -0,0 +1,8 @@
+public class PriceException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public PriceException(String errorMessage) {
+        super(errorMessage);
+    }
+
+}
